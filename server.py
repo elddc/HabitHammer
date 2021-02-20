706f900b00e06c2bdd7b05ec64be27b3d66af8d3
Glitch (habithammer) <none>
2021-02-20 22:03:16 +0000

    Added Flask backend

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route("/")
+def home():
+  return render_template('index.html')
+
+if __name__ == "__main__":
+  app.run(debug=True)
