b763ff2db87a700052752d31456df2695c6c50ee
elddc <emmachen2010@gmail.com>
2021-02-21 11:11:49 -0600

    redirect fixes

--- static/auth.js
+++ static/auth.js
@@ -4,7 +4,7 @@ function signUp(){
 		document.getElementById("pass").value)
 	.then(() => console.log("signup successful"))
 	.catch(e => console.log(e.message));
-	window.location.href = '/app/habits';
+	window.location.href = '/habits';
 }
 
 function logIn(){
@@ -13,10 +13,11 @@ function logIn(){
 		document.getElementById("pass").value)
 	.then(() => console.log("login successful"))
 	.catch(e => console.log(e.message));
-	window.location.href = '/app/habits';
+	window.location.href = '/habits';
 }
 
 function logOut(){
-	auth.signOut();
+	firebase.auth().signOut();
 	console.log("Signed out");
+	window.location.href = '/';
 }
