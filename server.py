6566830c74148e7653e4ea49cb203add14366dfe
elddc <emmachen2010@gmail.com>
2021-02-20 18:29:53 -0600

    added firebase auth

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route("/")
+@app.route('/')
 def home():
   return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def home():
+  return render_template('signup.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def home():
+  return render_template('login.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
